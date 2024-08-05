f845897b9a487bdb8ae5f82a9bf959f8f5e19a1f
AnthonySaimon <22202609@aluno.cotemig.com.br>
2024-08-05 06:02:37 -0300

    atualizar

--- ADM/Gerenciar_user_edt/index.php
+++ ADM/Gerenciar_user_edt/index.php
@@ -11,8 +11,9 @@
      <meta charset='utf-8'>
      <meta http-equiv='X-UA-Compatible' content='IE=edge'>
      <title>Aprendix</title>
-     <meta name='viewport' content='width=device-width, initial-scale=1'>
+     <meta name='viewport' content='width=device-width, initial-scale=1'>     
      <link rel='stylesheet' type='text/css' media='screen' href='main.css'>
+     <link rel='stylesheet' type='text/css' media='screen' href='main2.css'>
      <link rel="shortcut icon" href="../../imagem/Logo-aprendix.png" type="image/ico" />
 
      <link rel="preconnect" href="https://fonts.googleapis.com">
